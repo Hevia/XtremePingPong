d55a3f6b08411888fd06b3146de6ec1768f08a84
hevia <anthony@hevia.dev>
2025-02-17 22:46:31 -0800

    dash jumping + paddle jump

--- scenes/game objects/ball.gd
+++ scenes/game objects/ball.gd
@@ -1,9 +1,20 @@
 extends CharacterBody3D  
 class_name Ball
 
+@onready var colorswitcher_timer: Timer = %ColorswitcherTimer
+@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+
 const BASE_SPEED = 60.0  
 var ricochets_remaining = 100  
 var mass = 10.0
+
+var switch_colors = false
+var current_color: Color = Color.WHITE
+var next_color: Color = Color.WHITE
+
+func set_color(next_color: Color) -> void:
+	next_color = next_color
+	colorswitcher_timer.start()
 
 func apply_force(force: Vector3):
 	# F = ma, so a = F/m
--- scenes/game objects/player.gd
+++ scenes/game objects/player.gd
@@ -11,6 +11,9 @@ const HEAD_Y_CLAMP_UP = deg_to_rad(90)
 const CROUCHING_DEPTH = -0.5 # How much the head is dipping based on the camera
 const SLIDE_LENGTH_MAX = 1.0
 const B_SLIDE_SPEED = 10.0
+const B_DASH_SPEED = 12.0
+const DASH_LENGTH_MAX = 1.0
+const SLOW_TIME_TOGGLE_SPEED = 0.1
 
 var current_speed = 5.0
 var curr_jump_velocity = 4.5
@@ -20,6 +23,8 @@ var direction = Vector3.ZERO
 var freelook_tilt_amt = 5.0
 var slide_timer = 0.0
 var slide_vec = Vector2.ZERO
+var dash_timer = 0.0
+var dash_vec = Vector2.ZERO
 var last_velocity =  Vector3.ZERO
 
 # Head bobbing vars
@@ -41,7 +46,10 @@ var sprinting = false
 var crouching = false
 var freelooking = false
 var sliding = false
+var dashing = false
+var slow_time_toggle = false
 
+@onready var paddle_area_3d: Area3D = %PaddleArea3D
 @onready var camera_3d: Camera3D = %Camera3D
 @onready var freelook_pivot: Node3D = %FreelookPivot
 @onready var head: Node3D = %Head
@@ -50,17 +58,26 @@ var sliding = false
 @onready var crouching_ray_cast_check: RayCast3D = %CrouchingRayCastCheck
 @onready var head_bob_pivot: Node3D = %HeadBobPivot
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var paddle_area_3d: Area3D = $PaddleArea3D
+@onready var paddle_jump_raycast: RayCast3D = $FreelookPivot/Head/PaddleJumpRaycast
+
+const PADDLE_JUMP_VELOCITY = 5.0
+
 var hit_force = 100.0  # Adjust this to control hit strength
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	paddle_area_3d.area_entered.connect(on_paddle_area_entered)
-	
+
+func reset_time() -> void:
+	if slow_time_toggle:
+		Engine.time_scale = SLOW_TIME_TOGGLE_SPEED
+	else:
+		Engine.time_scale = 1.0
+
 func hitstop(time_scale, duration) -> void:
 	Engine.time_scale = time_scale
 	await(get_tree().create_timer(duration * time_scale).timeout)
-	Engine.time_scale = 1.0
+	reset_time()
 	
 func shake_screen():
 	pass
@@ -70,11 +87,17 @@ func on_paddle_area_entered(other_area: Area3D):
 		var hit_direction = calculate_hit_direction()
 		var force = hit_direction * hit_force
 		hitstop(0.05, 0.7)
+		shake_screen()
 		other_area.owner.apply_force(force)
 
 func calculate_hit_direction() -> Vector3:
 	return -global_transform.basis.z.normalized()
 
+func handle_paddle_jump():
+	if not is_on_floor() and paddle_jump_raycast.enabled and paddle_jump_raycast.is_colliding():
+		return true
+	else:
+		return false
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -91,18 +114,29 @@ func _input(event):
 		head.rotate_x(mouse_y_mov)
 		head.rotation.x = clamp(head.rotation.x, HEAD_Y_CLAMP_DOWN, HEAD_Y_CLAMP_UP)
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("primary") and not animation_player.is_playing():
 		animation_player.play("swing_paddle")
+	
+	if Input.is_action_just_pressed("slow time"):
+		slow_time_toggle = !slow_time_toggle
+		
+		if slow_time_toggle:
+			Engine.time_scale = SLOW_TIME_TOGGLE_SPEED
+		else:
+			Engine.time_scale = 1.0
 		
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	
+	if handle_paddle_jump():
+		print("we paddle jumping")
+		velocity.y = PADDLE_JUMP_VELOCITY
 	
 	# Crouching is dominant, we want it to overwrite sprint
-	if Input.is_action_pressed("crouch") || sliding:
+	if (Input.is_action_pressed("crouch") || sliding) && not dashing:
 		current_speed = lerp(current_speed, B_CROUCHING_SPEED, delta*lerp_speed)
 		head.position.y = lerp(head.position.y, CROUCHING_DEPTH, delta*lerp_speed)
 		crouching_collision_shape.disabled = false
@@ -115,6 +149,7 @@ func _physics_process(delta: float) -> void:
 			slide_vec = input_dir
 			freelooking = true
 		
+		dashing = false
 		walking = false
 		sprinting = false
 		crouching = true
@@ -124,7 +159,7 @@ func _physics_process(delta: float) -> void:
 		head.position.y = lerp(head.position.y, 0.0, delta*lerp_speed)
 		crouching = false
 		
-		if Input.is_action_pressed("sprint"):
+		if Input.is_action_pressed("sprint") && not dashing:
 			walking = false
 			sprinting = true
 			current_speed = lerp(current_speed, B_SPRINTING_SPEED, delta*lerp_speed)
@@ -146,6 +181,22 @@ func _physics_process(delta: float) -> void:
 		freelooking = false
 		camera_3d.rotation.z = lerp(camera_3d.rotation.z, 0.0, delta*lerp_speed)
 		freelook_pivot.rotation.y = lerp(freelook_pivot.rotation.y, 0.0, delta*lerp_speed)
+	
+	# Trigger dashing
+	if Input.is_action_pressed("dash") && not sliding && input_dir != Vector2.ZERO:
+		dashing = true
+		walking = false
+		sprinting = false
+		sliding = false
+		dash_timer = DASH_LENGTH_MAX
+		dash_vec = input_dir
+	
+	# Handle dashing
+	if dashing:
+		dash_timer -= delta
+		if dash_timer <= 0:
+			dash_timer = 0.0
+			dashing = false
 	
 	# Handle sliding
 	if sliding:
@@ -194,6 +245,7 @@ func _physics_process(delta: float) -> void:
 		# Use this to handle different landings, small, heavy, etc
 		if last_velocity.y <= -4.0:
 			pass # Play a small camera bump animation
+			
 	
 	# Handle air control
 	if is_on_floor():
@@ -206,6 +258,8 @@ func _physics_process(delta: float) -> void:
 	if sliding:
 		# Transform basis takes into account the rotation of the player
 		direction = (transform.basis * Vector3(slide_vec.x, 0.0, slide_vec.y)).normalized()
+	elif dashing:
+		direction = (transform.basis * Vector3(dash_vec.x, 0.0, dash_vec.y)).normalized()
 	
 	if direction:
 		velocity.x = direction.x * current_speed
@@ -216,6 +270,9 @@ func _physics_process(delta: float) -> void:
 			# add 0.1 so it never truly reaches 0
 			velocity.x = direction.x * (slide_timer + 0.1) * B_SLIDE_SPEED
 			velocity.z = direction.z * slide_timer * B_SLIDE_SPEED
+		elif dashing:
+			velocity.x = direction.x * (dash_timer + 0.1) * B_DASH_SPEED
+			velocity.z = direction.z * (dash_timer + 0.1) * B_DASH_SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
