9ce5c80607d5bc99714698cc6064693a456c0306
hevia <anthony@hevia.dev>
2025-05-03 16:03:59 -0700

    target and trampoline models

--- scenes/game objects/level_objects/jump_pad.gd
+++ scenes/game objects/level_objects/jump_pad.gd
@@ -8,8 +8,6 @@ extends Node3D
 func _ready() -> void:
 	jump_pad_area_3d.body_entered.connect(on_jump_pad_area_entered)
 
-
 func on_jump_pad_area_entered(other_body: Node3D):
-	print("Jump pad entered!")
 	if other_body and other_body is CharacterBase:
 		(other_body as CharacterBase).force_jump(jump_pad_strength)
