794eaf6df0c9f2b9b442b71d3bb8e8280b99a6a3
hevia <anthony@hevia.dev>
2025-05-20 14:58:37 -0700

    fix color issue in enemyt attack spark vfx

--- scenes/vfx/enemy_attack_spark.gd
+++ scenes/vfx/enemy_attack_spark.gd
@@ -1,11 +1,13 @@
 class_name EnemyAttackSpark extends GPUParticles3D
 
 @onready var timer: Timer = Timer.new()
+@onready var circle_indicator_spark: GPUParticles3D = %CircleIndicatorSpark
 
 func _ready() -> void:
 	timer.wait_time = self.lifetime
 	add_child(timer)
 	emitting = true
+	circle_indicator_spark.emitting = true
 	timer.timeout.connect(on_timer_timeout)
 
 func on_timer_timeout() -> void:
